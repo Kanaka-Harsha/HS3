fd2277e1a52ecd025f8c49c2fe6774fdfa8f9516
Kanakadandila Harsha <harshakanakadandila@gmail.com>
2026-01-29 15:29:49 +0530

    Displayes all the files user has in s3

---
+++ src/dataDisplayerCard.jsx
@@ -0,0 +1,11 @@
+import './dataDisplayerCard.css'
+import { useState } from 'react'
+
+function DataDisplayer()
+{
+    return (
+        <div className='dataDisplayer'>
+            
+        </div>
+    )
+}
